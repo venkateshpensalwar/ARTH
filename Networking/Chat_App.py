2228c878246b23c7ba72d9c55f48cd1f9d2d1e26
venatesh <venkateshpensalwar@gmail.com>
2021-03-08 10:05:55 +0530

    UDP Task Done

--- Networking/Chat_App.py
+++ Networking/Chat_App.py
@@ -1,6 +1,8 @@
 import socket as soc
 import os 
 import threading
+import pyfiglet
+
 
 ## your ip 
 SenderIP = input("Enter Sender IP address: ")
@@ -14,27 +16,42 @@ ReciverPort = int(input("Enter Reciver port: "))
 ## Creating UDP Socket
 ReadySocket = soc.socket(soc.AF_INET,soc.SOCK_DGRAM)
 
-## only for Reciving messages
+## only for Reciving messages 
 ReadySocket.bind((SenderIP,SenderPort))
 
+
+os.system('cls')
+print(pyfiglet.figlet_format('Walkers Chat'))
+
+
+## Sending message function
 def SendMsg():
         while True:
-                Message = input('Enter Message: ')
+                Message = input('{}:'.format(SenderIP))
+                print("\n")
                 if(Message == 'quite' or Message == 'bye' or Message == 'exit'):    
                        Message = 'Your Friend is offline'    
                        ReadySocket.sendto(Message.encode(),(ReciverIP,ReciverPort))
                        os._exit(1)
                 else:
                      ReadySocket.sendto(Message.encode(),(ReciverIP,ReciverPort))
+                    
 
-
+## Receving message function
 def RecvMsg():
         while True:
                 Msg = ReadySocket.recvfrom(100)
-                print(Msg[0].decode())
+                print("\n\t\t\t\t" + Msg[1][0] + ":" + Msg[0].decode())
+               
+
+## Created 2 Threads for sending and reciving msg parllerl
+SendMsg = threading.Thread(target=SendMsg)
+RecvMsg = threading.Thread(target=RecvMsg)
 
 
-SendMsg()
-RecvMsg()
+## starting thread 
+SendMsg.start()
+RecvMsg.start()
+
 
 
